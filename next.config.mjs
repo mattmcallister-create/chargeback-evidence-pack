d1379800ca05056a92fb24ee90f35133ea4531dd
mattmcallister-create <mattmcallistermarketing@gmail.com>
2026-05-27 13:01:45 -0700

    fix(config): remove ignoreBuildErrors and ignoreDuringBuilds

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,6 @@ const nextConfig = {
   images: {
     domains: [],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 }
 
 export default nextConfig
